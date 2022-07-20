172c5e3fb681515e924ca2fdf5da3fde0424123a
Andrew-Radchikov <superprobasite@yandex.ru>
2022-07-20 10:40:20 +0300

    init task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,45 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const openBrackets=[];
+  const closeBrackets=[];
+  const stek=[];
+
+  bracketsConfig.forEach(element => {
+      openBrackets.push(element[0]);
+      closeBrackets.push(element[1]);
+    });
+
+  const brackets=str.split('');
+  
+
+  for(let value of brackets) {
+    if(openBrackets.includes(value,0)){
+      if(closeBrackets.includes(value,0)){
+          if(stek[stek.length-1] == value){
+            stek.pop();
+          }
+          else{
+            stek.push(value);
+          }
+      }
+      else{
+        stek.push(value);
+      }
+    }
+    else{
+      if(stek.length==0){
+        return false;
+        break;
+      }
+      else if(openBrackets[closeBrackets.indexOf(value,0)] == stek[stek.length-1]){
+        stek.pop();
+      }
+        else{
+          return false;
+          break;
+        }
+    }
+  }
+ 
+  return stek.length ==0;
+ 
 }
